35de6813001964395f02384d5a618ef7c24fda52
darkHorseWhiteSigma <42654526+darkHorseWhiteSigma@users.noreply.github.com>
2018-12-07 14:09:23 +0000

    Delete Film.java

--- StreamService/StreamService/Film.java
+++
@@ -1,7 +0,0 @@
-import java.util.*;
-public class Film extends Media{
-    
-    public Film(String title, int year, ArrayList<String> genre, double rating){
-        super(title,year,genre,rating);
-    }
-}
